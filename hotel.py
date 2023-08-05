b765863007b20e1ec53b4a5c335f25f79569f795
RachaponKjr <rachapon.navy@gmail.com>
2023-08-05 19:49:32 +0700

    5/8/2023

--- hotel.py
+++ hotel.py
@@ -6,12 +6,28 @@ class HotelManagementSystem:
     def __init__(self,root):
         self.root=root
         self.root.title("Hotel Management System")
-        self.root.geometry("1550x800+0+0")
+        self.root.geometry("1550x800")
 
 
+        msg = Message( root, text = "เข้าสู่ระบบ",font=28,width="100")
+        msg.place(relx = 0.5, rely = 0.2, anchor = CENTER)
+
+        inputUser = Entry(root,width=50)
+        inputUser.place(relx = 0.5, rely = 0.25, anchor = CENTER)
 
 
+        inputPassword = Entry(root,width=50, show="*")
+        inputPassword.place(relx = 0.5, rely = 0.3, anchor = CENTER)
+
+
+        btnSubmit = Button(root,text="LOGIN",width=40)
+        btnSubmit.place(relx = 0.5, rely = 0.35, anchor = CENTER)
+
+         
 if __name__=="__main__":
-    root=Tk()
-    obj=HotelManagementSystem(root)
-    root.mainloop()
+        root=Tk()
+        obj=HotelManagementSystem(root)
+        root.mainloop()
+    
+        
+        
