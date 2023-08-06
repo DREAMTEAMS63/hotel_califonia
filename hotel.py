67f770d2abc0eab4ab6fd7262785e6247673fd77
RachaponKjr <rachapon.navy@gmail.com>
2023-08-06 20:30:16 +0700

    6/8/2023

--- hotel.py
+++ hotel.py
@@ -1,38 +1,49 @@
-from tkinter import*
-
-
-
-class HotelManagementSystem:
+from tkinter import *
+from pages.appPageOne import appPage1
+class MainPage:
     def __init__(self,root):
         self.root=root
         self.root.title("Hotel Management System")
-        self.root.geometry("1550x800")
-
+        # DATA TEST
+        testuser = "test"
+        testpass = "test01"
+        # DATA TEST
+        # CHACK LOGIN TEST
+        def clicksend():
+                us = username.get()
+                pw = password.get()
+                if(us == testuser):
+                      if( pw == testpass):
+                            self.root.destroy()
+                            appPage1()
+                            print("OK! รหัสถูกต้อง")
+                      else:
+                            print('NOT OK! รหัสไม่ถูกต้อง')
+                else:
+                      print("กรุณาใส่ USERNAME ให้ถูกต้อง")
+        # CHACK LOGIN TEST
+        username= StringVar()
+        password = StringVar()
 
         msg = Message( root, text = "เข้าสู่ระบบ",font=28,width="100")
         msg.place(relx = 0.5, rely = 0.2, anchor = CENTER)
 
-        inputUser = Entry(root,width=50)
+        inputUser = Entry(root,width=50, textvariable=username)
         inputUser.place(relx = 0.5, rely = 0.25, anchor = CENTER)
 
 
-        inputPassword = Entry(root,width=50, show="*")
+        inputPassword = Entry(root,width=50, show="*",textvariable=password)
         inputPassword.place(relx = 0.5, rely = 0.3, anchor = CENTER)
 
-
-        btnSubmit = Button(root,text="LOGIN",width=40)
+        btnSubmit = Button(root,text="LOGIN",width=40,command=clicksend)
         btnSubmit.place(relx = 0.5, rely = 0.35, anchor = CENTER)
 
          
 if __name__=="__main__":
         root=Tk()
-        obj=HotelManagementSystem(root)
+        object = MainPage(root)
+        root.wm_geometry("500x500")
         root.mainloop()
-        #####################################################
-#################################################
-#################################################
-#################################################
-
 
         
         
---
+++ pages/appPageOne.py
@@ -0,0 +1,50 @@
+from tkinter import *
+import tkinter as tk
+
+
+def appPage1():
+    app = Tk()
+    app.geometry("500x500")
+    app.title("test")
+
+    class Page(tk.Frame):
+        def __init__(self, *args, **kwargs):
+            tk.Frame.__init__(self, *args, **kwargs)
+        def show(self):
+            self.lift()
+
+    class Page1(Page):
+        def __init__(self, *args, **kwargs):
+            Page.__init__(self, *args, **kwargs)
+            label = tk.Label(self, text="This is page 1")
+            label.pack(side="top", fill="both", expand=True)
+
+    class Page2(Page):
+        def __init__(self, *args, **kwargs):
+            Page.__init__(self, *args, **kwargs)
+            label = tk.Label(self, text="This is page 2")
+            label.pack(side="top", fill="both", expand=True)
+
+    class MainView(tk.Frame):
+        def __init__(self, *args, **kwargs):
+            tk.Frame.__init__(self, *args, **kwargs)
+            p1 = Page1(self)
+            p2 = Page2(self)
+
+            buttonframe = tk.Frame(self)
+            container = tk.Frame(self)
+            buttonframe.pack(side="top", fill="x", expand=False)
+            container.pack(side="top", fill="both", expand=True)
+
+            p1.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+            p2.place(in_=container, x=0, y=0, relwidth=1, relheight=1)        
+
+            b1 = tk.Button(buttonframe, text="Page 1", command=p1.lift)
+            b2 = tk.Button(buttonframe, text="Page 2", command=p2.lift)        
+
+            b1.pack(side="left")
+            b2.pack(side="left")        
+
+            p1.show()
+
+    app.mainloop()
